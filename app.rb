5e68d4ab5214fcb5a453e4449a504c80824adb13
Masako Gray <masakogray@hotmail.co.uk>
2021-06-07 16:49:52 +0100

    cat picture added

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
-# require "sinatra/reloader" if development?
+require "sinatra/reloader" if development?
 
 get '/' do
   "Hello"
 end
 
 get '/secret' do
-  'This is a secret page'
+  'This is a secret page that you can only see'
 end 
+
+get '/cat' do
+  "<div style='border: 6px dashed yellow'>
+    <img src='https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*'>
+   </div>"
+  
+end
